2c4d13325298226241706a5b43d1c6d0b91e2f5a
hwandev12 <husan.ahmedov777@yandex.com>
2024-02-26 21:08:10 +0500

    update ajax

--- apps/story/views.py
+++ apps/story/views.py
@@ -29,35 +29,8 @@ def is_ajax(request):
 
 
 def story_detail(request, storyID):
-    if is_ajax(request=request):
-        print("ajax")
-        try:
-            story = Story.objects.get(id=storyID)
-            if Friendship.objects.filter(Q(user1=request.user) | Q(user2=request.user)):
-                for user in Friendship.objects.filter(Q(user1=request.user) | Q(user2=request.user)):
-                    m_stories = Story.objects.filter(
-                        user=request.user)  # mening storyim
-                    o_stories = Story.objects.exclude(id__in=m_stories.values_list("id", flat=True)).filter(
-                        Q(user=user.user1) |
-                        Q(user=user.user2)
-                    ).order_by("created_at")
-
-                    stories = list(m_stories) + list(o_stories)
-                    story_ids = [st.id for st in stories]
-                    # Remove the current story ID from the list
-                    story_ids = [id for id in story_ids if id != story.id]
-                    next_story_id = story_ids[0] if story_ids else None
-
-                    if not story_ids:
-                        return JsonResponse({'next_story_id': None})
-        except Story.DoesNotExist:
-            return JsonResponse({'next_story_id': None})
-
-        return JsonResponse({'next_story_id': next_story_id})
-    else:
-        story = Story.objects.get(id=storyID)
-        print('sefdsd')
-        context = {
-            "story": story
-        }
-        return render(request, 'includes/story_content.html', context)
+    story = Story.objects.get(id=storyID)
+    context = {
+        "story": story
+    }
+    return render(request, 'includes/story_content.html', context)
